9ed00f2f6d1ffede66bb46eb782879ac274a6d39
KarthikeyaTadiparti <kartikeyatadiparti@gmail.com>
2025-02-16 17:24:45 +0530

    updated mongoose

--- server/models/db.js
+++ server/models/db.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 async function main() {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
     // console.log(process.env.MONGODB_URL);
 }
 main()
     .then(() => console.log("DB connected successfully"))
     .catch((err) => console.log(err));
-
-    
