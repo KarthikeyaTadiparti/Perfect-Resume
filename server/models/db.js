7f276d7204ab7e5e52a92034b7f118dccabf947a
KarthikeyaTadiparti <kartikeyatadiparti@gmail.com>
2025-02-16 16:03:51 +0530

    added mongodb altas integration

--- server/models/db.js
+++ server/models/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    await mongoose.connect(process.env.MONGODB_URL);
+    // console.log(process.env.MONGODB_URL);
 }
 main()
     .then(() => console.log("DB connected successfully"))
